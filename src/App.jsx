d92a9c1521f34673411fc71c68b61b03a7255a75
Dido2002 <didoguzelev@gmail.com>
2024-03-13 08:33:17 +0200

    AddingSomePredefinedClases,Colors&Fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import './App.css'
-
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">Hello World!</h1>     
-  )
+  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
 }
 
-export default App
+export default App;
