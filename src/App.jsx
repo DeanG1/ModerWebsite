3a61cdbaa15070fe7b5ff08ccf87a407d5080293
Dido2002 <didoguzelev@gmail.com>
2024-03-14 08:07:50 +0200

    CreatedReusableButtonComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
-}
+import Button from "./Components/Button";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
