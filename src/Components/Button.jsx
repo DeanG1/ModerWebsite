3fc454daec8fdd4521f0d80c9dd9478655899888
Dido2002 <didoguzelev@gmail.com>
2024-03-15 08:32:46 +0200

    FinishedHeaderComponent

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,17 +1,16 @@
-import React from "react";
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className, href, onClick, children, px, white }) => {
   const classes = `button relative inline-flex items-center justify-center h-11 transition-color hover:text-color-1 ${
     px || "px-7"
   } 
-  ${white ? "text-n-8" : "text-n-1"} $ {className || ""}`;
+  ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
   //   So, overall, this part dynamically sets the text color of the button based on
   //the white prop, and it allows for additional custom classes to be added via the className prop.
 
   const spanClasses = "relative z-10";
   const renderButton = () => (
-    <button className={classes}>
+    <button onClick={onClick} className={classes}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
