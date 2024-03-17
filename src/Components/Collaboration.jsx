855f41a4238d36418b3e53f08f6bad22adb6301f
Dido2002 <didoguzelev@gmail.com>
2024-03-17 08:41:29 +0200

    AddingCurves&RightSideCollaborationComponent

--- src/Components/Collaboration.jsx
+++ src/Components/Collaboration.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Section from "./Section";
 import check from "../assets/check.svg";
 import { collabContent, collabText } from "../Constants/collabContent";
-import { Button } from "react-scroll";
+import Button from "../Components/Button";
+import { collabApps } from "../Constants/collabApps";
+import { brainwave, brainwaveSymbol } from "../assets";
+import { list } from "postcss";
+import { LeftCurve, RightCurve } from "./design/Collaboration";
 
 const Collaboration = () => {
   return (
@@ -23,20 +27,60 @@ const Collaboration = () => {
                     className="body-2 ml-5"
                     alt={check}
                   />
-                  <h6>{item.title}</h6>
+                  <h6 className="body-2 ml-5">{item.title}</h6>
                 </div>
                 {item.text && (
-                  <p className="body-2 mt-3 text-n-4">{item.text}</p>
+                  <p className="body-2 mt-3 ml-5 text-n-4">{item.text}</p>
                 )}
               </li>
             ))}
           </ul>
-          <Button>Try it now</Button>
+          <Button className="ml-5">Try it now</Button>
         </div>
         <div className="lg:ml-auto xl:w-[30rem] mt-4">
-          <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
+          <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
             {collabText}
           </p>
+          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+            <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+              <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
+                <div className="flex items-center justify-center h-full w-full bg-n-8 rounded-full">
+                  <img
+                    src={brainwaveSymbol}
+                    width={48}
+                    height={48}
+                    alt="brainwave"
+                  />
+                </div>
+              </div>
+            </div>
+            <ul>
+              {collabApps.map((app, index) => (
+                <li
+                  className={`absolute top-0
+                left-1/2 h-1/2 -ml-[1.6rem] origin-bottom
+                rotate-${index * 45}`}
+                  key={app.id}
+                >
+                  <div
+                    className={`relative -top-[1.6rem]
+                  flex w-[3.2rem] height-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl
+                  -rotate-${index * 45}`}
+                  >
+                    <img
+                      className="m-auto"
+                      width={app.width}
+                      height={app.height}
+                      src={app.icon}
+                      alt={app.title}
+                    />
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <LeftCurve />
+            <RightCurve />
+          </div>
         </div>
       </div>
     </Section>
--- src/Components/design/Collaboration.jsx
+++ src/Components/design/Collaboration.jsx
@@ -10,8 +10,8 @@ export const RightCurve = () => {
 
 export const LeftCurve = () => {
   return (
-    <div className="hidden absolute top-1/2 right-full w-[32.625rem] -mt-1 mr-10 pointer-events-none xl:block">
-      <img src={curve1} width={522} height={182} alt="Curve 1" />
+    <div className="hidden absolute top-1/2 right-full w-[32.625rem] lg:w-[38.625rem] -mt-1 xl:-mt-10 mr-10 pointer-events-none xl:block">
+      <img src={curve1} width={580} height={282} alt="Curve 1" />
     </div>
   );
 };
